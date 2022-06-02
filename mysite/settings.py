351cf21215567fefee483f6de20ad471b632b191
Hupperich-Manuel <mahupperich@gmail.com>
2022-06-02 08:49:39 +0200

    New

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 #SECRET_KEY = 'SECRET_KEY'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = bool( os.environ.get('DJANGO_DEBUG', True) )
+#DEBUG = bool(os.environ.get('DJANGO_DEBUG', True) )
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
