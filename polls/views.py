a9e4853c4fa2bd823f12ea83886663caa2e54278
Hupperich-Manuel <mahupperich@gmail.com>
2022-06-08 13:38:52 +0200

    changes

--- polls/views.py
+++ polls/views.py
@@ -227,13 +227,14 @@ def modelling(request):
         for key in places.keys():
             selected_experience.append(str(Question.objects.get(pk=places[key][0])))
             selected_experience.append(str(places[key][1]))
+            selected_experience.append(str(places[key][2]))
             selected_experience.append(str(places[key][3]))
 
 
         select_place = []
         for index, building in enumerate(selected_experience):
 
-             if index%3 == 0:
+             if index%4 == 0:
 
                 selected_choice = Customer(username_id=User.objects.get(username=request.user),
                                     email= request.user.email,
@@ -261,7 +262,7 @@ def modelling(request):
                                     question_text19 = answer19,
                                     question_text20 = answer20,
                                     building_id = Question.objects.get(building=selected_experience[index]),
-                                    dot_product = selected_experience[index+2],
+                                    dot_product = selected_experience[index+3],
                                     opinion = "No Opinion"
                                     )
                 select_place.append(selected_choice)
